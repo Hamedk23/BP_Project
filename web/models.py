b1b49e674fddbcbe6c6099aeaf1abae6e2a0f720
Hamedk23 <hamed.karimi.81.23@gmail.com>
2021-03-02 23:41:03 +0330

    2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+class login(models.Model):
+    email=models.CharField()
+    key=models.CharField()
+    
